0672fc8186aa300c610e28121b3287fa81fddc9d
Philippe Desautels <philippe.desautels@gmail.com>
2014-02-28 10:23:37 -0500

    Trying to fix my install issues.

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 # check to see if there's an up to date git repo in the .oh-my-zsh dir and clone it if not.
+if [ ! -d ~/.oh-my-zsh/.git ]; then
+    mkdir -p ~/.oh-my-zsh/
+    pushd ~/.oh-my-zsh
+    git clone https://github.com/robbyrussell/oh-my-zsh.git .
+    # let's copy our modified powerline theme
+    popd
+    cp powerline.zsh-theme ~/.oh-my-zsh/themes/ 
+fi
 
 if [[ ! `which zsh` ]]; then
     echo "Make sure you install zsh on your system first"
@@ -16,7 +24,7 @@ install() {
 	cp -aR ./$1 ~/$1
 }
 
-for item in .zshrc .vimrc .tmux.conf .gitconfig .vim .oh-my-zsh
+for item in .zshrc .vimrc .tmux.conf .gitconfig .vim
 do
 	backup $item
 	echo "Backed up $item."
