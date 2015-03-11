b8dc154a85db0f02553430a6adebb0c6174fd1ce
Philippe Desautels <philippe.desautels@gmail.com>
2015-03-10 21:50:56 -0400

    Quick brew cask provisioning script.

---
+++ provision-cask.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+brew cask install alfred firefox hipchat macdown sketchup vagrant virtualbox xld dropbox google-chrome iterm2 namechanger skype vagrant-manager viscosity filezilla gpgtools lisanet-gimp sequel-pro transmission vimr vlc
