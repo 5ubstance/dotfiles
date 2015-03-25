40810831d8476f6a6a77a5248544f06c7011cdf5
Philippe Desautels <philippe.desautels@gmail.com>
2015-03-25 00:10:12 -0400

    Adds brew packages and move to line-by-line.

---
+++ provision-brew.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+#TODO Make sure brew is installed
+brew install \
+  caskroom/cask/brew-cask \
+  flac \
+  jq \
+  lsusb \
+  openssl \
+  pv \
+  rpm2cpio \
+  watch \
+  curl \
+  gdb \
+  keybase \
+  miniupnpc \
+  ossp-uuid \
+  qt \
+  swftools \
+  wget \
+  gdbm \
+  lame \
+  p7zip \
+  tmux \
+  x264 \
+  e2fsprogs \
+  gettext \
+  libevent \
+  readline \
+  tree \
+  xvid \
+  boost \
+  faac \
+  git \
+  libmagic \
+  nmap \
+  pkg-config \
+  unrar \
+  xz \
+  ffmpeg \
+  node \
+  rpm \
+  vim \
+  zsh
--- provision-cask.sh
+++ provision-cask.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-brew cask install alfred firefox hipchat macdown sketchup vagrant virtualbox xld dropbox google-chrome iterm2 namechanger skype vagrant-manager viscosity filezilla gpgtools lisanet-gimp sequel-pro transmission vimr vlc
+#TODO Make sure brew cask is installed
+
+brew cask install \
+  alfred \
+  firefox \
+  hipchat \
+  macdown \
+  sketchup \
+  vagrant \
+  virtualbox \
+  xld \
+  dropbox \
+  google-chrome \
+  iterm2 \
+  namechanger \
+  skype \
+  vagrant-manager \
+  viscosity \
+  filezilla \
+  gpgtools \
+  lisanet-gimp \
+  sequel-pro \
+  transmission \
+  vimr \
+  vlc
